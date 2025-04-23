9efda83cdac046c4103191c57af44020f269e57e
Brijesh8866 <brijesh8866p@gmail.com>
2025-04-23 11:18:13 +0530

    update mobile number

--- src/components/NutritionWhatsappHeaderBtn.jsx
+++ src/components/NutritionWhatsappHeaderBtn.jsx
@@ -12,7 +12,7 @@ function sendToWhatsApp(text, option) {
     }
   }
 
-  let url = `https://api.whatsapp.com/send?phone=+917874894727&text=${encodeURIComponent(
+  let url = `https://api.whatsapp.com/send?phone=+918320077993&text=${encodeURIComponent(
     text
   )}`;
   window.open(url, "_blank");
